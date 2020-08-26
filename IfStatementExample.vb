c4397020856d46c63b938004f34e3936a4e67c34
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-08-26 08:54:00 -0600

    added if then statement

--- IfStatementExample.vb
+++ IfStatementExample.vb
@@ -1,6 +1,30 @@
-﻿Module IfStatementExample
+﻿Option Strict On
+Option Explicit On
+
+Module IfStatementExample
 
     Sub Main()
+        Dim userInput As String
+        Dim userNumber As Integer
+
+        'If 3 > 5 Then
+        'Console.WriteLine("Yep")
+        'Else
+        'Console.WriteLine("Nope")
+        'End If
+        userInput = Console.ReadLine()
+        userNumber = CInt(userInput)
+
+        If userNumber > 5 Then
+            Console.WriteLine("Bigger than 5")
+        ElseIf userNumber < 5 Then
+            Console.WriteLine("Smaller than 5")
+        ElseIf userNumber = 5 Then
+            Console.WriteLine("It is 5")
+        Else
+            Console.WriteLine("Its weird")
+        End If
+        Console.ReadLine()
 
     End Sub
 
